cf321c1b1199a1280d80117c50b0a6351a7e2a25
rw <rw@jjk.is>
2024-06-28 21:02:49 -0700

    disable garnix cache

--- flake.nix
+++ flake.nix
@@ -76,13 +76,13 @@
               settings = {
                 experimental-features = [ "nix-command" "flakes" ];
                 substituters = [
-                  "https://cache.garnix.io"
+                  # "https://cache.garnix.io"
                   "https://nix-community.cachix.org"
                   "https://cache.nixos.org/"
                 ];
                 trusted-public-keys = [
                   "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-                  "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+                  # "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
                 ];
               };
             };
