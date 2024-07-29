70282706ac54b317a2d67a6f955c07adb2c737ba
rw <rw@jjk.is>
2024-07-29 11:45:27 -0700

    add prometheus to dev

--- modules/flakery/dev.nix
+++ modules/flakery/dev.nix
@@ -1,3 +1,14 @@
 { modulesPath, ... }: {
   imports = [ "${modulesPath}/virtualisation/amazon-image.nix" ];
+  services.prometheus = {
+    enable = true;
+    port = 9090;
+    exporters = {
+      node = {
+        enable = true;
+        enabledCollectors = [ "systemd" ];
+        port = 9002;
+      };
+    };
+  };
 }
